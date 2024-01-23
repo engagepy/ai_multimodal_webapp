a061e6a7a727e79606c0fe9949d5424b06cbd444
Zorawar Purohit <42845567+engagepy@users.noreply.github.com>
2024-01-24 01:12:39 +0530

    send button text removed

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
             <svg width="20" height="20" viewBox="0 0 20 20">
               <path d="M2.925 5.025L9.18333 7.70833L2.91667 6.875L2.925 5.025ZM9.175 12.2917L2.91667 14.975V13.125L9.175 12.2917ZM1.25833 2.5L1.25 8.33333L13.75 10L1.25 11.6667L1.25833 17.5L18.75 10L1.25833 2.5Z" />
             </svg>
-            <span className='hidden origin:block font-semibold text-sm ml-2'>Send</span>
+            {/* <span className='hidden origin:block font-semibold text-sm ml-2'>Send</span> */}
           </button>
         </form>
         <Footer />
