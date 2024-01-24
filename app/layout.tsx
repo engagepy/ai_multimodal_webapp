87c502c328e19c9aef7d9b1ebbb5d75d904ac62b
Zorawar Purohit <42845567+engagepy@users.noreply.github.com>
2024-01-24 05:37:20 +0530

    opengraphs added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import { Metadata } from 'next'
+import Head from 'next/head';
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "AI Kavach",
   description: "Mental Health by Kavach AI",
 };
@@ -12,6 +12,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={GeistSans.variable}>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content="./logo.png" />
+        <meta property="og:image:alt" content="Kavach Mental Health App Logo" />
+        <meta name="twitter:image" content="./logo.png" />
+      </Head>
       <body>{children}</body>
     </html>
   );
