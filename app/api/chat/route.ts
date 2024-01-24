a98c7126e90599573526bc6a0b0bc467ed97ae2e
Zorawar Purohit <42845567+engagepy@users.noreply.github.com>
2024-01-25 00:24:24 +0530

    obsolete code removed

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,40 +1,14 @@
 import OpenAI from 'openai';
 import {OpenAIStream, StreamingTextResponse} from 'ai';
-// import {AstraDB} from "@datastax/astra-db-ts";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-// const astraDb = new AstraDB(process.env.ASTRA_DB_APPLICATION_TOKEN, process.env.ASTRA_DB_ID, process.env.ASTRA_DB_REGION, process.env.ASTRA_DB_NAMESPACE);
-
 export async function POST(req: Request) {
   try {
     const {messages, useRag, llm, similarityMetric} = await req.json();
 
-    const latestMessage = messages[messages?.length - 1]?.content;
-
-    // let docContext = '';
-    // if (useRag) {
-    //   const {data} = await openai.embeddings.create({input: latestMessage, model: 'text-embedding-ada-002'});
-
-    //   const collection = await astraDb.collection(`chat_${similarityMetric}`);
-
-    //   const cursor= collection.find(null, {
-    //     sort: {
-    //       $vector: data[0]?.embedding,
-    //     },
-    //     limit: 5,
-    //   });
-      
-    //   const documents = await cursor.toArray();
-      
-    //   docContext = `
-    //     START CONTEXT
-    //     ${documents?.map(doc => doc.content).join("\n")}
-    //     END CONTEXT
-    //   `
-    // }
     const ragPrompt = [
       {
         role: 'system',
