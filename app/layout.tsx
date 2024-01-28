d470fe632c1c46d89453db6466711d5f08be644d
Zorawar Purohit <42845567+engagepy@users.noreply.github.com>
2024-01-29 01:52:38 +0530

    login added via clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from '@clerk/nextjs'
 import Head from 'next/head';
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
@@ -11,6 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en" className={GeistSans.variable}>
       <Head>
         <title>{metadata.title}</title>
@@ -23,5 +25,6 @@ export default function RootLayout({ children }) {
       </Head>
       <body>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
