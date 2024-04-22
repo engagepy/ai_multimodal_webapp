84e9c6a25cf66c1f48b1c173a52d768dedb4c89f
Zorawar Purohit <42845567+engagepy@users.noreply.github.com>
2024-04-23 00:07:34 +0530

    back to chat api

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,109 +1,28 @@
 import OpenAI from "openai";
-import { NextResponse } from "next/server";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-export const maxDuration = 300;
-export const runtime = 'nodejs';
 
-async function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-class CircuitBreaker {
-  func: any;
-  threshold: number;
-  cooldown: number;
-  failures: number;
-  lastTime: number;
-  constructor(func, threshold = 3, cooldown = 10000) {
-    this.func = func;
-    this.threshold = threshold;
-    this.cooldown = cooldown;
-    this.failures = 0;
-    this.lastTime = Date.now();
-  }
-
-  async call(...args) {
-    if (this.failures >= this.threshold && Date.now() < this.lastTime + this.cooldown) {
-      throw new Error('Circuit is open');
-    }
-    try {
-      const result = await this.func(...args);
-      this.failures = 0; // Reset on success
-      return result;
-    } catch (error) {
-      this.failures++;
-      this.lastTime = Date.now();
-      if (this.failures >= this.threshold) {
-        setTimeout(() => this.failures = 0, this.cooldown); // Reset failures count after cooldown
-      }
-      throw error;
-    }
-  }
-}
-
-async function retryOperation(operation, retries = 5, delayLength = 500) {
-  let lastError = null;
-  for (let i = 0; i < retries; i++) {
-    try {
-      return await operation();
-    } catch (error) {
-      lastError = error;
-      if (i < retries - 1) {
-        await delay(delayLength);
-        delayLength *= 2; // Exponential backoff
-      }
-    }
-  }
-  throw lastError;
-}
-
-const createThread = async (threadId) => {
-  if (threadId) {
-    return openai.beta.threads.retrieve(threadId);
-  } else {
-    return openai.beta.threads.create();
-  }
-};
-
-export async function POST(req) {
-  const circuitBreaker = new CircuitBreaker(retryOperation);
+export async function POST(req: Request) {
   try {
-    const { messages, threadId } = await req.json();
-
-    let thread = await circuitBreaker.call(() => createThread(threadId));
-    const newThread = !threadId;
-
-    const lastMessage = messages[messages.length - 1];
-    const message = await circuitBreaker.call(() => openai.beta.threads.messages.create(thread.id, {
-      role: lastMessage.role,
-      content: lastMessage.content,
-    }));
-
-    const assistant = await circuitBreaker.call(() => openai.beta.assistants.update(process.env.OPENAI_ASSISTANT_ID, {}));
-    let run = await circuitBreaker.call(() => openai.beta.threads.runs.createAndPoll(thread.id, {
-      assistant_id: assistant.id,
-    }));
-
-    while (run.status === "in_progress" || run.status === "queued") {
-      // await delay(500);
-      run = await circuitBreaker.call(() => openai.beta.threads.runs.retrieve(thread.id, run.id));
-    }
-
-    const messagesResult = await circuitBreaker.call(() => openai.beta.threads.messages.list(thread.id));
-    const responseContent = messagesResult.data[0].content[0]?.text?.value || "";
-
-    return new NextResponse(JSON.stringify({
-      content: responseContent,
-      data: {
-        threadId: thread.id,
-        newThread: newThread,
+    const { messages } = await req.json();
+    const systemPrompt = [
+      {
+        role: "system",
+        content: `You are an expert mental health consultant. Keep your responses and langaguage aligned for an adoloscent age group. Generate confidence in users so they can share their deep rooted issues. Do not discuss any information, topic or subject outside the scope of mental health. If the conversation becomes too negative and user gives negative responses only, respond like this "This is serious. Please call 1098, or download the Kavach App from Play store or App store.  While Kavach professionals connect with you shortly, let us discuss this."`,
       },
-    }));
-  } catch (error) {
-    console.error('Error:', error.message);
-    return new NextResponse(JSON.stringify({ error: "Failed to process request" }), { status: 500 });
+    ];
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-4",
+      stream: true,
+      messages: [...systemPrompt, ...messages],
+    });
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
+  } catch (e) {
+    throw e;
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ import ThemeButton from "../components/ThemeButton";
 import useConfiguration from "./hooks/useConfiguration";
 import { UserButton } from "@clerk/nextjs";
 import { Mic, SendHorizontal } from "lucide-react";
-import { ClipLoader } from "react-spinners";
 import { start } from "repl";
 import { on } from "events";
 
@@ -18,17 +17,12 @@ let recorder = null;
 
 export default function Home() {
   const [streamActive, setStreamActive] = useState(true);
-  const [threadId, setThreadId] = useState(null);
-  const [Loading, setLoading] = useState(false);
 
   const onFinish = () => {
     setStreamActive(false);
   };
   const { append, messages, input, handleInputChange, handleSubmit } = useChat({
     onFinish,
-    body: {
-      threadId: threadId,
-    },
   });
   const { useRag, llm, similarityMetric, setConfiguration } =
     useConfiguration();
@@ -43,25 +37,9 @@ export default function Home() {
 
   useEffect(() => {
     scrollToBottom();
-
-    if (
-      messages[messages.length - 1]?.role === "assistant" &&
-      messages[messages.length - 1]?.content
-    ) {
-      const msg = JSON.parse(messages[messages.length - 1].content);
-      messages[messages.length - 1].content = msg.content;
-      console.log(msg);
-      // setThreadId(messages[messages.length - 1].content.threadId);
-      setThreadId(msg.data.threadId);
-      setLoading(false);
-      // console.log(threadId + " threadId");
-      // console.log(messages[messages.length - 1].content);
-      console.log(messages[messages.length - 1]);
-    }
   }, [messages]);
 
   const handleSend = (e) => {
-    setLoading(true);
     handleSubmit(e, { options: { body: { useRag, llm, similarityMetric } } });
     setStreamActive(true);
   };
@@ -72,7 +50,6 @@ export default function Home() {
       content: promptText,
       role: "user",
     };
-    setLoading(true);
     append(msg, { options: { body: { useRag, llm, similarityMetric } } });
   };
 
@@ -192,7 +169,7 @@ export default function Home() {
             <div className="flex justify-between">
               <div className="flex items-center gap-2">
                 <Image
-                  src="/green_logo.png"
+                  src="/logo.png"
                   className="w-18 h-8 md:w-25 md:h-10"
                   width="100"
                   height="100"
@@ -202,7 +179,7 @@ export default function Home() {
               </div>
               <div className="flex gap-1">
                 <ThemeButton />
-                {/* <button onClick={() => setConfigureOpen(true)}>
+                <button onClick={() => setConfigureOpen(true)}>
                   <svg
                     width="24"
                     height="25"
@@ -212,7 +189,7 @@ export default function Home() {
                   >
                     <path d="M19.14 13.4006C19.18 13.1006 19.2 12.7906 19.2 12.4606C19.2 12.1406 19.18 11.8206 19.13 11.5206L21.16 9.94057C21.34 9.80057 21.39 9.53057 21.28 9.33057L19.36 6.01057C19.24 5.79057 18.99 5.72057 18.77 5.79057L16.38 6.75057C15.88 6.37057 15.35 6.05057 14.76 5.81057L14.4 3.27057C14.36 3.03057 14.16 2.86057 13.92 2.86057H10.08C9.83999 2.86057 9.64999 3.03057 9.60999 3.27057L9.24999 5.81057C8.65999 6.05057 8.11999 6.38057 7.62999 6.75057L5.23999 5.79057C5.01999 5.71057 4.76999 5.79057 4.64999 6.01057L2.73999 9.33057C2.61999 9.54057 2.65999 9.80057 2.85999 9.94057L4.88999 11.5206C4.83999 11.8206 4.79999 12.1506 4.79999 12.4606C4.79999 12.7706 4.81999 13.1006 4.86999 13.4006L2.83999 14.9806C2.65999 15.1206 2.60999 15.3906 2.71999 15.5906L4.63999 18.9106C4.75999 19.1306 5.00999 19.2006 5.22999 19.1306L7.61999 18.1706C8.11999 18.5506 8.64999 18.8706 9.23999 19.1106L9.59999 21.6506C9.64999 21.8906 9.83999 22.0606 10.08 22.0606H13.92C14.16 22.0606 14.36 21.8906 14.39 21.6506L14.75 19.1106C15.34 18.8706 15.88 18.5506 16.37 18.1706L18.76 19.1306C18.98 19.2106 19.23 19.1306 19.35 18.9106L21.27 15.5906C21.39 15.3706 21.34 15.1206 21.15 14.9806L19.14 13.4006ZM12 16.0606C10.02 16.0606 8.39999 14.4406 8.39999 12.4606C8.39999 10.4806 10.02 8.86057 12 8.86057C13.98 8.86057 15.6 10.4806 15.6 12.4606C15.6 14.4406 13.98 16.0606 12 16.0606Z" />
                   </svg>
-                </button> */}
+                </button>
                 <UserButton />
               </div>
             </div>
@@ -229,19 +206,16 @@ export default function Home() {
           </div>
           <div className="flex-1 relative overflow-y-auto my-4 md:my-6">
             <div className="absolute w-full overflow-x-hidden">
-              {messages.map((message, index) => {
-                return (
-                  <div key={index}>
-                    {/* {console.log(message)} */}
-                    <Bubble
-                      ref={messagesEndRef}
-                      key={`message-${index}`}
-                      content={message} // Add the missing 'content' prop
-                      isActive={streamActive}
-                    />
-                  </div>
-                );
-              })}
+              {messages.map((message, index) => (
+                <div key={index}>
+                  <Bubble
+                    ref={messagesEndRef}
+                    key={`message-${index}`}
+                    content={message}
+                    isActive={streamActive}
+                  />
+                </div>
+              ))}
             </div>
           </div>
           {!messages ||
@@ -257,23 +231,18 @@ export default function Home() {
             />
 
             {/* Send Button */}
-
             <button
               type="submit"
               className="chatbot-send-button flex rounded-md items-center justify-center px-2.5"
             >
-              {Loading ? (
-                <ClipLoader size={20} className=" text-white bg-white" />
-              ) : (
-                <svg width="20" height="20" viewBox="0 0 20 20">
-                  <path
-                    fill="yellow"
-                    stroke="black"
-                    strokeWidth="0.5"
-                    d="M2.925 5.025L9.18333 7.70833L2.91667 6.875L2.925 5.025ZM9.175 12.2917L2.91667 14.975V13.125L9.175 12.2917ZM1.25833 2.5L1.25 8.33333L13.75 10L1.25 11.6667L1.25833 17.5L18.75 10L1.25833 2.5Z"
-                  />
-                </svg>
-              )}
+              <svg width="20" height="20" viewBox="0 0 20 20">
+                <path
+                  fill="yellow"
+                  stroke="black"
+                  strokeWidth="0.5"
+                  d="M2.925 5.025L9.18333 7.70833L2.91667 6.875L2.925 5.025ZM9.175 12.2917L2.91667 14.975V13.125L9.175 12.2917ZM1.25833 2.5L1.25 8.33333L13.75 10L1.25 11.6667L1.25833 17.5L18.75 10L1.25833 2.5Z"
+                />
+              </svg>
 
               {/* <SendHorizontal className="chatbot-text-secondary-inverse" /> */}
             </button>
