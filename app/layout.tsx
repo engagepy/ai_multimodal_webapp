53d27da33073a9986cc9e15c3d8cb751f4ea9e8d
Zorawar Purohit <42845567+engagepy@users.noreply.github.com>
2024-05-02 03:51:39 +0530

    header refactored

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 
 export const metadata = {
-  title: "Kavach A.I.",
-  description: "Your Mental Health Safe Space",
+  title: "ATZ AI Solutions",
+  description: "Astra Techz Customises & Deploys AI Solutions",
 };
 
 
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:image" content="./logo.png" />
-        <meta property="og:image:alt" content="Kavach Mental Health App Logo" />
+        <meta property="og:image:alt" content="ATZ AI Solutions" />
         <meta name="twitter:image" content="./logo.png" />
       </Head>
       <body>{children}</body>
