15019bdbc4f4440fe476da2cd0e8b4dd99696577
Zorawar Purohit <42845567+engagepy@users.noreply.github.com>
2024-05-06 05:19:28 +0530

    signin restored position

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { SignUp } from "@clerk/nextjs";
+import { SignIn, SignUp } from "@clerk/nextjs";
 import Head from "next/head";
 import "tailwindcss/tailwind.css";
 import Image from "next/image";
@@ -116,16 +116,7 @@ export default function EnterpriseLandingPage() {
               clipRule="evenodd"
             />
           </svg>
-          <div className="flex w-full justify-center">
-          <div className="w-full max-w-xs mx-auto">
-            <h2 className="text-xl font-bold text-[#efb849] mb-4">
-              Explore Our Healthcare AI Demo
-            </h2>
-            <SignUp />
-            
-          </div>
-        </div>
-          <div className="mt-6 ">
+          <div className="mt-6">
   <h2 className="my-8 text-2xl font-bold text-[#efb849] md:text-4xl">
     Why <span className="text-transparent bg-clip-text bg-gradient-to-br text-white">Use AI ?</span>
   </h2>
@@ -168,7 +159,15 @@ export default function EnterpriseLandingPage() {
         </div>
 
         {/* Adjusted SignIn box */}
-        
+        <div className="flex w-full lg:w-1/3 justify-center p-4">
+          <div className="w-full max-w-xs mx-auto">
+            <h2 className="text-xl font-bold text-[#efb849] mb-4">
+              Explore Our Healthcare AI Demo
+            </h2>
+            <SignUp />
+            
+          </div>
+        </div>
       </div>
 
       {/* Features Section */}
